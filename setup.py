95509f310706b8c2bd54816bad01018d97985ef0
smugay <85663797+smugay@users.noreply.github.com>
2023-08-26 01:59:48 -0400

    "For interacting txts" 💔

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 setup(
     name='discord.py',
     author='smugay',
-    description='Python wrapper for interacting txts',
+    description='Python wrapper for interacting with txts',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
